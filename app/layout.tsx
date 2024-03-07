73fa6afd23691339c4423c1a74f65642f2e6b41f
Adem <ademakyasar@gmail.com>
2024-03-07 18:44:05 -0500

    working login/out with auth0

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import NavMenu from "@/components/NavMenu"
 const inter = Inter({ subsets: ["latin"] })
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { UserProvider } from "@auth0/nextjs-auth0/client"
 
 export const metadata: Metadata = {
   title: "Personal Pantry",
@@ -17,15 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-primary-foreground`}>
-        <NavMenu />
-        <Avatar className="absolute top-10 right-20">
-          <AvatarImage src="https://github.com/shadcn.png" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
-
-        {children}
-      </body>
+      <UserProvider>
+        <body className={`${inter.className} bg-primary-foreground`}>
+          <NavMenu />
+          {children}
+        </body>
+      </UserProvider>
     </html>
   )
 }
--- components/NavMenu.tsx
+++ components/NavMenu.tsx
@@ -2,12 +2,23 @@
 import React from "react"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar"
+import { UserProfile, useUser } from "@auth0/nextjs-auth0/client"
+import { Button } from "./ui/button"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import { Table, TableBody, TableCaption, TableCell, TableRow } from "./ui/table"
 
 function NavMenu() {
   const pathname = usePathname()
-
+  const user = useUser()
+  console.log(user)
   const pathActive = (href: string) => href === pathname
   const navLinks = [
+    { href: "/", name: "Home" },
     { href: "/search", name: "Food Search" },
     { href: "/fridge", name: "Fridge" },
     { href: "/menu", name: "Menu" },
@@ -15,20 +26,59 @@ function NavMenu() {
   ]
 
   return (
-    <div
-      className={`flex gap-2 shadow-md p-1 rounded-md border border-secondary w-fit m-auto mt-10`}
-    >
-      {navLinks.map((e, idx) => (
-        <div
-          className={`rounded-md p-1 px-3 hover:bg-secondary cursor-pointer ${
-            pathActive(e.href) ? "bg-secondary" : ""
-          }`}
-          key={idx}
-        >
-          <Link href={e.href}>{e.name}</Link>
-        </div>
-      ))}
-    </div>
+    <>
+      {user.user ? (
+        <Popover>
+          <PopoverTrigger asChild>
+            <Avatar className="absolute top-10 right-32 w-10 h-10 rounded-full overflow-hidden cursor-pointer">
+              <AvatarImage src={user.user.picture!} />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </PopoverTrigger>
+          <PopoverContent>
+            <ProfileTable user={user.user} />
+          </PopoverContent>
+        </Popover>
+      ) : (
+        <Button className="absolute top-10 right-20">
+          <a href="/api/auth/login">Login</a>
+        </Button>
+      )}
+
+      <div
+        className={`flex gap-2 shadow-md p-1 rounded-md border border-secondary w-fit m-auto mt-10`}
+      >
+        {navLinks.map((e, idx) => (
+          <div
+            className={`rounded-md p-1 px-3 hover:bg-secondary cursor-pointer ${
+              pathActive(e.href) ? "bg-secondary" : ""
+            }`}
+            key={idx}
+          >
+            <Link href={e.href}>{e.name}</Link>
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
+
+function ProfileTable({ user }: { user: UserProfile }) {
+  return (
+    <Table>
+      <TableCaption>
+        {user.name}
+        <br />
+        {user.email}
+      </TableCaption>
+      <TableBody>
+        <TableRow>
+          <TableCell className="font-medium text-center cursor-pointer rounded-md">
+            <a href="/api/auth/logout">Logout</a>
+          </TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
   )
 }
 
