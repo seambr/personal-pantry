fe78ff1ab6126a801226f040bdb6043dfa6a563a
Adem <ademakyasar@gmail.com>
2024-03-08 15:26:58 -0500

    checkout before commit

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -5,12 +5,14 @@ import { ScrollArea } from "@/components/ui/scroll-area"
 import { FoodItem, SearchResponse } from "@/interfaces/FoodInterfaces"
 
 function SearchResults({ results }: { results: SearchResponse }) {
-  return (
-    <ScrollArea className="h-[650px] w-5/6 rounded-md p-4 gap-2 m-auto mt-5">
+  return results?.foods.length > 0 ? (
+    <ScrollArea className="h-[650px] w-5/6 rounded-md p-4 gap-2 m-auto mt-5 flex justify-center">
       {results?.foods.map((r, idx) => (
         <SearchCard key={idx} foodItem={r} />
       ))}
     </ScrollArea>
+  ) : (
+    <div className="w-full text-center mt-20">Search for an item to begin.</div>
   )
 }
 
