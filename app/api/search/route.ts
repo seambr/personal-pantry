b47b7dd687b2b6298bf7d984506dafc47f065546
seambr <ademakyasar@gmail.com>
2024-04-18 14:34:36 -0400

    Revert "removed vercel app for now"
    This reverts commit d6c5222ccbac1c8f974ea342f1833df5755e88fc.

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,14 +1,15 @@
-import { NextRequest, NextResponse } from "next/server";
-import { SearchCriteria } from "@/interfaces/FoodInterfaces";
+import { NextRequest, NextResponse } from "next/server"
+import { SearchCriteria } from "@/interfaces/FoodInterfaces"
 export async function POST(req: NextRequest) {
-  const r = await req.json();
-  const query: SearchCriteria = r.query;
+  const r = await req.json()
+  const query: SearchCriteria = r.query
 
-  const url = `https://api.nal.usda.gov/fdc/v1/foods/search?`;
+  const url = `https://api.nal.usda.gov/fdc/v1/foods/search?`
 
   const params = new URLSearchParams({
-    api_key: process.env.API_KEY!,
-  });
+    // TODO: SWAP OUT API KEY WITH ENV VARIABLE
+    api_key: "DEMO_KEY",
+  })
 
   const api_res = await fetch(url + params, {
     method: "POST",
@@ -21,8 +22,8 @@ export async function POST(req: NextRequest) {
       pageSize: query.pageSize < 50 ? query.pageSize : 25,
       pageNumber: query.pageNumber,
     }),
-  });
-  const data = await api_res.json();
+  })
+  const data = await api_res.json()
 
-  return NextResponse.json(data);
+  return NextResponse.json(data)
 }
